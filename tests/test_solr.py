d57a2defe091b58b3b748cade25d6475eec7b6b5
aherlihy <anna.herlihy@10gen.com>
2016-03-31 14:51:34 +0200

    solr test passing with solr.xml

--- tests/test_solr.py
+++ tests/test_solr.py
@@ -93,12 +93,8 @@ class TestSolr(SolrTestCase):
         """Tests insert
         """
 
-        result = self.solr_conn.search('*:*')
-        print "before res=", list(result)
         self.conn['test']['test'].insert_one({'name': 'paulie'})
         assert_soon(lambda: sum(1 for _ in self.solr_conn.search('*:*')) > 0)
-        result = self.solr_conn.search('*:*')
-        print "after res=", list(result)
         result_set_1 = list(self.solr_conn.search('name:paulie'))
         self.assertEqual(len(result_set_1), 1)
         result_set_2 = self.conn['test']['test'].find_one()
@@ -121,12 +117,17 @@ class TestSolr(SolrTestCase):
         test_data = "test_insert_file test file"
         id = fs.put(test_data, filename="test.txt", encoding='utf8')
         assert_soon(lambda: sum(1 for _ in self.solr_conn.search('*:*')) > 0)
+
         res = list(self.solr_conn.search('content:*test_insert_file*'))
+        if not res:
+            res = list(self.solr_conn.search('_text_:*test_insert_file*'))
         self.assertEqual(len(res), 1)
         doc = res[0]
         self.assertEqual(doc['filename'], "test.txt")
         self.assertEqual(doc['_id'], str(id))
-        self.assertIn(test_data.strip(), doc['content'][0].strip())
+        content = doc.get('content', doc.get('_text_', None))
+        self.assertTrue(content)
+        self.assertIn(test_data.strip(), content[0].strip())
 
     def test_remove_file(self):
         """Tests removing a gridfs file
